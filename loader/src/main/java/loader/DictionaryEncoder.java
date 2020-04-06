95b29e68e467143c3ff54a8c01d07232abf89e69
demetriushal <dimitrioshal@gmail.com>
2020-04-06 22:01:26 +0300

    corrected dictionary encoding bugs

--- loader/src/main/java/loader/DictionaryEncoder.java
+++ loader/src/main/java/loader/DictionaryEncoder.java
@@ -16,7 +16,7 @@ public class DictionaryEncoder extends Loader {
     final private String mixedTable = "mixed_temp";
     final private String distinctTable = "distinct_temp";
     final private String keyPrefix = "dd";
-    final private String rowedTable = "rowed_temp";
+    final private String rowedTable = "dictionary_so";
     final private String final1 = "final1";
 
     final private boolean removeTempTables = false;
@@ -36,16 +36,16 @@ public class DictionaryEncoder extends Loader {
     }
 
     public void load() {
+        logger.info("----- Starting Dictionary Encoder ------");
 
-        String dropTable = String.format("DROP TABLE IF EXISTS %1$s", tableSpecifier);
+
         String dropFinalTable = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, finalTable);
-        String drop1 = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, finalTable);
+        String drop1 = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, rowedTable);
         String drop2 = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, mixedTable);
         String drop3 = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, distinctTable);
         String drop4 = String.format("DROP TABLE IF EXISTS %1$s.%2$s", dbName, final1);
 
 
-        spark.sql(dropTable);
         spark.sql(dropFinalTable);
         spark.sql(drop1);
         spark.sql(drop2);
@@ -54,20 +54,26 @@ public class DictionaryEncoder extends Loader {
 
 
 
-        String insertSubjects = String.format("CREATE TABLE %1$s.2$s  AS SELECT %3$s %4$s",
+        String insertSubjects = String.format("CREATE TABLE %1$s.%2$s  AS (SELECT %3$s FROM %4$s)",
                 dbName, mixedTable, column_name_subject,tableSpecifier);
-        String insertObjects = String.format("INSERT INTO TABLE %1$s.2$s  AS SELECT %3$s %4$s WHERE OType=2",
+        String insertObjects = String.format("INSERT INTO TABLE %1$s.%2$s SELECT %3$s FROM %4$s WHERE OType=2",
                 dbName, mixedTable, column_name_object,tableSpecifier);
-        String createDistinctTable = String.format("CREATE TABLE %1$s.%2$s  AS SELECT DISTINCT(s) FROM %1$s.%4$s",
+        String createDistinctTable = String.format("CREATE TABLE %1$s.%2$s  AS (SELECT DISTINCT(s) FROM %1$s.%3$s)",
                 dbName, distinctTable, mixedTable);
-        String addRowNumber = String.format("CREATE TABLE %1$s.%2$s  AS SELECT s as key, concat('%3$s', (row_number() over (order by s))) value FROM %1$s.%4$s",
+        String addRowNumber = String.format("CREATE TABLE %1$s.%2$s  AS (SELECT s as key, concat('%3$s', (row_number() over (order by s))) value FROM %1$s.%4$s)",
                 dbName, rowedTable, keyPrefix, distinctTable);
-        String createFinalPart1 = String.format("CREATE TABLE %1$s.%2$s as SELECT t1.value as s,t.p,t2.value as o , t.OType=2 FROM prost_test.dict_final t1, prost_test.dict_final t2, prost_test.triples t WHERE t1.key=t.s AND t2.key=t.o"
-                ,dbName, final1);
+        String createFinalPart1 = String.format("CREATE TABLE %1$s.%2$s as (SELECT t1.value as s,t.p,t2.value as o, t.OType FROM %1$s.%3$s t1, %1$s.%3$s t2, %1$s.%4$s t WHERE t.OType=2 AND t1.key=t.s AND t2.key=t.o)"
+                ,dbName, final1, rowedTable, tableName);
         String createFinal = String.format("CREATE TABLE %1$s.%2$s AS (SELECT * FROM %1$s.%3$s UNION SELECT * FROM %1$s.%4$s WHERE OType=1)",
                 dbName, finalTable, final1, tableName);
 
-
+        logger.info("---- DEC ----- Running the following:");
+        logger.info(insertSubjects);
+        logger.info(insertObjects);
+        logger.info(createDistinctTable);
+        logger.info(addRowNumber);
+        logger.info(createFinalPart1);
+        logger.info(createFinal);
 
         spark.sql(insertSubjects);
         spark.sql(insertObjects);
@@ -77,11 +83,10 @@ public class DictionaryEncoder extends Loader {
         spark.sql(createFinal);
 
         if (this.removeTempTables) {
-            spark.sql(drop1);
+            //spark.sql(drop1); Never drop this one, as it contains the encoding
             spark.sql(drop2);
             spark.sql(drop3);
             spark.sql(drop4);
-            spark.sql(dropTable);
 
         }
 
